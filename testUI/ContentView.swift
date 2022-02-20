c73ac86e6970a0d5ff17f4e2640e29bc8909d0f3
Adham <adam.bolotov@gmail.com>
2022-02-20 01:43:13 +0100

    Initial Commit

---
+++ testUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testUI
+//
+//  Created by user213083 on 2/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testUI/testUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testUIApp.swift
+//  testUI
+//
+//  Created by user213083 on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct testUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
